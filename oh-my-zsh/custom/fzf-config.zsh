978094c6416e605f8eceb995ab482ab068cf1614
koeir <kianjohnban@gmail.com>
2026-06-09 16:31:59 +0800

    log: fzf-config.zsh: removed header when alt-c

--- oh-my-zsh/custom/fzf-config.zsh
+++ oh-my-zsh/custom/fzf-config.zsh
@@ -1,2 +1,2 @@
 export FZF_DEFAULT_OPTS="--style full --preview '$HOME/.config/fzf/koeir/fzf-preview.sh {}' --bind 'focus:transform-header:file --brief {}'"
-export FZF_ALT_C_OPTS="--no-preview"
+export FZF_ALT_C_OPTS="--no-preview --bind 'focus:transform-header:'"
