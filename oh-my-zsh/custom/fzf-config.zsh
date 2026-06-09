3bef806242a681c8ae76363d48e1b56255bf0d02
koeir <kianjohnban@gmail.com>
2026-06-09 16:50:52 +0800

    log: fzf-config.zsh: better focus:transform-header:file

--- oh-my-zsh/custom/fzf-config.zsh
+++ oh-my-zsh/custom/fzf-config.zsh
@@ -1,2 +1,6 @@
-export FZF_DEFAULT_OPTS="--style full --preview '$HOME/.config/fzf/koeir/fzf-preview.sh {}' --bind 'focus:transform-header:file --brief {}'"
+export FZF_DEFAULT_OPTS="
+  --style full \
+  --preview '$HOME/.config/fzf/koeir/fzf-preview.sh {}' \
+  --bind 'focus:transform-header: [ -e "{}" ] && file --brief {}' \
+"
 export FZF_ALT_C_OPTS="--no-preview --bind 'focus:transform-header:'"
