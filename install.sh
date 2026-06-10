dca78a5b194b8664e5ca6bef3b77635f0a97a295
koeir <kianjohnban@gmail.com>
2026-06-10 21:37:25 +0800

    log: install.sh: cleanup

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rsync_omz=1
+rsyncOmz=true
 
-if [ ! -f "/usr/bin/rsync" ]; then 
+if ! command -v rsync; then 
     echo "[$0] rsync is not installed. Unable to sync ./oh-my-zsh/ config"
-    rsync_omz=0
+    rsyncOmz=false
 fi
 
 echo "~/.zshrc -> ~/.zshrc{,.pre-koeir-config}"
@@ -17,23 +17,35 @@ if [ ! -d "$fzfconfigdir" ]; then
     echo "mkdir $fzfconfigdir"
     if ! mkdir -p $fzfconfigdir; then
         echo "exiting script"
-        exit
+        exit 1
     fi
 fi
 
 echo "./config/fzf/koeir/* -> $fzfconfigdir"
 cp ./config/fzf/koeir/* $fzfconfigdir
 
+# backup omzdir first
+echo "./oh-my-zsh -> ~/.oh-my-zsh.pre-koeir"
+if $rsyncOmz; then
+    if ! rsync -r ${omzdir}{/*,.pre-koeir} --delete; then
+        echo "exiting script"
+        exit 1
+    fi
+else
+    if ! cp -r ${omzdir}{,.pre-koeir}; then
+        echo "exiting script"
+        exit 1
+    fi
+fi
+
 omzdir="$HOME/.oh-my-zsh"
-if [[ $rsync_omz -eq 1 ]]; then
+if $rsyncOmz; then
     if [ ! -d "$omzdir" ]; then
         echo "${omzdir}: Directory not found"
     fi
 
-    echo "./oh-my-zsh -> ~/.oh-my-zsh.pre-koeir"
-    rsync -r ${omzdir}{,.pre-koeir} --delete
     echo "rsync -rv ./oh-my-zsh/* $omzdir"
-	rsync -r ./oh-my-zsh/* $omzdir
+	rsync -r ./oh-my-zsh/* $omzdir --delete
 fi
 
 echo ""
